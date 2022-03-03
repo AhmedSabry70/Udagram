6532f2f092f899b15e93aab5fc132cf38420bb9e
Ahmed Sabry <infosabry70@gmail.com>
2022-03-03 22:17:32 +0200

    final --v

---
+++ udagram-api/src/sequelize.ts
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
+
+export const sequelize = new Sequelize({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+  port: config.db_port,
+
+  dialect: "postgres",
+  storage: ":memory:",
+});
+
+//////////
+//export const sequelize = new Sequelize(`postgres://${config.username}:${config.password}@${config.host}:${config.pg_port}/${config.database}`);
