547a2d27f903d0a5b5c22251c66c0ba2441304c5
Ahmed Sabry <infosabry70@gmail.com>
2022-03-05 14:00:02 +0200

    deploy command

--- udagram-frontend/bin/deploy.sh
+++ udagram-frontend/bin/deploy.sh
@@ -1,5 +1,4 @@
-cd ..
-aws s3 cp --recursive --acl public-read ./www s3://udagram-frontend-udacity-g5/
+cd ..  && aws s3 cp --recursive --acl public-read ./www s3://udagram-frontend-udacity-g5/
 
 
 
